bab3373e2ab5a92a515adfe70eed060a2586866d
Guðjón Karl Arnarson <kromby@hress.org>
2020-08-20 21:17:39 +0000

    reading menu items from api

--- src/pages/frame/menu.js
+++ src/pages/frame/menu.js
@@ -1,8 +1,8 @@
 import React, {useEffect, useState, useRef} from 'react';
 import { Link } from 'react-router-dom';
-// import config from 'react-global-configuration';
+import config from 'react-global-configuration';
 import { useAuth } from '../../context/auth';
-// import axios from "axios";
+import axios from "axios";
 
 /**
  * Hook that alerts clicks outside of the passed ref
@@ -31,29 +31,41 @@ function useOutsideAlerter(ref, visible, callback) {
 
 const Menu = (propsData) => {
     const {authTokens} = useAuth();
-    const [data, setData] = useState({isLoading: false, userID: 0});
+    const [data, setData] = useState({isLoading: false, menuItems: null, userID: 0});
     const wrapperRef = useRef(null);
     useOutsideAlerter(wrapperRef, propsData.visible, propsData.onClick);
 
     useEffect(() => {
         const getMenuData = async  () => {
-            // if(authTokens !== undefined){ 
-            //     try {
-            //         var url = config.get('path') + '/api/hardhead/' + propsData.id + '/ratings?code=' + config.get('code');                    
-            //         const response = await axios.get(url, {
-            //             headers: {'Authorization': 'token ' + authTokens.token}               
-            //         })
-            //         setData({ratings: response.data, isLoading: false, visible: true})
-            //     }
-            //     catch(e) {
-            //         console.error(e);
-            //         setData({isLoading: false, visible: false});
-            //     }
-            // }
-            if(authTokens !== undefined) {
-                var userID = localStorage.getItem("userID");
-                setData({userID});
-            }
+            
+            console.log("getMenuData");
+            console.log(window.location.pathname);
+
+                try {
+                    var url = config.get('path') + '/api/menus?navigateUrl=~' + window.location.pathname + '&fetchChildren=true&code=' + config.get('code');                    
+                    if(authTokens !== undefined){ 
+                        const response = await axios.get(url, {
+                            headers: {'Authorization': 'token ' + authTokens.token}               
+                        });
+                        var userID = localStorage.getItem("userID");
+                        setData({menuItems: response.data, isLoading: false, visible: true, userID: userID})
+                        console.log("getMenuData - data retrieved");
+                        console.log(response.data);
+                        console.log(data.visible);
+                    }
+                    else {
+                        const response = await axios.get(url);
+                        setData({menuItems: response.data, isLoading: false, visible: true})
+                        console.log("getMenuData - data retrieved");
+                        console.log(response.data);
+                        console.log(data.visible);
+                    }                    
+                }
+                catch(e) {
+                    console.error(e);
+                    setData({isLoading: false, visible: false});
+                }                    
+            console.log(data.visible);
         }
         getMenuData();
     }, [propsData, authTokens])
@@ -69,12 +81,18 @@ const Menu = (propsData) => {
 
                 <section>
                 <ul className="links" onClick={() => propsData.onClick()}>
-                    {/* <li>
-                    <a href="#">
-                        <h3>Lorem ipsum</h3>
-                        <p>Feugiat tempus veroeros dolor</p>
-                    </a>
-                    </li>  */}
+                    {data.visible ? 
+                        data.menuItems.map(item => 
+                        item.Public ?
+                        <li>
+                            <Link to={"/" + item.Link.Href}>
+                                <h3>{item.Name}</h3>
+                                <p>{item.Description}</p>
+                            </Link>
+                        </li>: null
+                    ) : null                    
+                    }
+
                     {authTokens !== undefined ?
                     <li>
                         <Link to={"/hardhead?userID=" + data.userID}>
@@ -87,7 +105,10 @@ const Menu = (propsData) => {
 
                 <section>
                 <ul className="actions stacked">
-                    <li><a href="#" className="button large fit">Innskráning</a></li>
+                    {authTokens !== undefined ?
+                        <li onClick={() => propsData.onClick()}><a href="#" className="button large fit">Útskráning</a></li> :
+                        <li><a href="#" className="button large fit">Innskráning</a></li>
+                    }
                 </ul>
                 </section>
             
