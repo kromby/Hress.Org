0c87354f1ef2f01bfe54bd31b77fa2dcca86fafb
Guðjón Karl Arnarson <kromby@hress.org>
2022-06-04 17:04:09 +0000

    Trying to put startup file back in

--- api/Ez.Hress.FunctionsApi/Startup.cs
+++ api/Ez.Hress.FunctionsApi/Startup.cs
@@ -1,46 +1,46 @@
-﻿//using Azure.Data.Tables;
-//using Ez.Hress.Hardhead.DataAccess;
-//using Ez.Hress.Hardhead.UseCases;
-//using Microsoft.Azure.Functions.Extensions.DependencyInjection;
-//using Microsoft.Extensions.Configuration;
-//using Microsoft.Extensions.DependencyInjection;
-//using Microsoft.Extensions.Logging;
-//using System;
-//using System.Collections.Generic;
-//using System.Configuration;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using Azure.Data.Tables;
+using Ez.Hress.Hardhead.DataAccess;
+using Ez.Hress.Hardhead.UseCases;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//[assembly: FunctionsStartup(typeof(Ez.Hress.FunctionsApi.Startup))]
+[assembly: FunctionsStartup(typeof(Ez.Hress.FunctionsApi.Startup))]
 
-//namespace Ez.Hress.FunctionsApi
-//{
-//    public class Startup : FunctionsStartup
-//    {
+namespace Ez.Hress.FunctionsApi
+{
+    public class Startup : FunctionsStartup
+    {
 
-//        public override void Configure(IFunctionsHostBuilder builder)
-//        {
-//            var config = new ConfigurationBuilder()
-//                .AddJsonFile("host.json", optional: true)
-//                .AddEnvironmentVariables()
-//                .Build();
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            //var config = new ConfigurationBuilder()
+            //    .AddJsonFile("host.json", optional: true)
+            //    .AddEnvironmentVariables()
+            //    .Build();
 
-//            //builder.Services.AddLogging();
+            //builder.Services.AddLogging();
 
-//            ConfigureServices(builder.Services, config);
-//        }
+            //ConfigureServices(builder.Services, config);
+        }
 
-//        public void ConfigureServices(IServiceCollection services, IConfigurationRoot config)
-//        {                      
-//            var connectionString = config["TableConnectionString"];
+        //public void ConfigureServices(IServiceCollection services, IConfigurationRoot config)
+        //{
+        //    var connectionString = config["TableConnectionString"];
 
 
-//            services.AddSingleton(new TableClient(connectionString, "HardheadNominations"));
-//            services.AddScoped<AwardInteractor>();
-//            services.AddScoped<IAwardDataAccess, AwardTableDataAccess>();
+        //    services.AddSingleton(new TableClient(connectionString, "HardheadNominations"));
+        //    services.AddScoped<AwardInteractor>();
+        //    services.AddScoped<IAwardDataAccess, AwardTableDataAccess>();
 
-//            //services.AddA
-//        }
-//    }
-//}
+        //    //services.AddA
+        //}
+    }
+}
