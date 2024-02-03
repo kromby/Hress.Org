9c6d13beeb0e35d99cf9aeb0894de7e84a9f6f72
Guðjón Karl Arnarson <kromby@hress.org>
2024-02-03 22:11:19 +0000

    Meta image chnaged and news now using new api for images

--- src/pages/news/singleNews.js
+++ src/pages/news/singleNews.js
@@ -26,22 +26,18 @@ const SingleNews = () => {
         }
     }, [])
 
-
     return (
         <div id="main">
-
-
             {news ?
                 [
                     <Helmet key="helmet">
                         <title>{news.name} | Hress.Org</title>
                         <meta name="description" content={news.name} />
                         <meta property="og:title" content={news.name} />
-                        <meta property="og:image" content={news.image ? config.get('path') + news.image.href + "?code=" + config.get('code') : null} />
-                        <meta property="og:image:secure_url" content={news.image ? config.get('path') + news.image.href + "?code=" + config.get('code') : null} />
+                        <meta property="og:image" content={news.image && news.image.id ? config.get('apiPath') + news.image.href : null} />
+                        <meta property="og:image:secure_url" content={news.image && news.image.id ? config.get('apiPath') + news.image.href : null} />
                         <meta property="og:image:width" content="1000" />
                         <meta property="og:image:height" content="563" />
-                        {/* <meta property="og:description" content=""></meta> */}
                     </Helmet>,
                     <Post key={news.id}
                         id={news.id}
