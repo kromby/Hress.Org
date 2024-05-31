805640b2bcfcbe245325002d827d9f5420dacf3c
Guðjón Karl Arnarson <kromby@hress.org>
2024-05-31 21:19:55 +0000

    Dunno

--- src/pages/hardhead/rules/rulesSections.js
+++ src/pages/hardhead/rules/rulesSections.js
@@ -1,40 +1,40 @@
-import { useState, useEffect } from 'react';
-import config from 'react-global-configuration';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import config from "react-global-configuration";
+import axios from "axios";
 
-const RulesSections = ({id}) => {
-    const [data, setData] = useState({ rules: null, isLoading: false, visible: false })
+const RulesSections = ({ id }) => {
+  const [data, setData] = useState({
+    rules: null,
+    isLoading: false,
+    visible: false,
+  });
 
-    useEffect(() => {
-        const getAwards = async () => {
-            const url = `${config.get('apiPath')}/api/hardhead/rules/${id}`;
+  useEffect(() => {
+    const getAwards = async () => {
+      const url = `${config.get("apiPath")}/api/hardhead/rules/${id}`;
 
-            try {
-                setData({ isLoading: true });
-                const response = await axios.get(url);
-                setData({ rules: response.data, isLoading: false, visible: true });
-            } catch (e) {
-                console.error(e);
-                setData({ isLoading: false, visible: false });
-            }
-        }
+      try {
+        setData({ isLoading: true });
+        const response = await axios.get(url);
+        setData({ rules: response.data, isLoading: false, visible: true });
+      } catch (e) {
+        console.error(e);
+        setData({ isLoading: false, visible: false });
+      }
+    };
 
-        if (!data.rules) {
-            getAwards();
-        }
-    }, [id])
+    if (!data.rules) {
+      getAwards();
+    }
+  }, [id]);
 
-    return (
-        <ol>
-            {data.visible ?
-                data.rules.map((rule) =>
-                    <li key={rule.id}>
-                        {rule.name}
-                    </li>
-                ) :
-                null}
-        </ol>
-    )
-}
+  return (
+    <ol>
+      {data.visible
+        ? data.rules.map((rule) => <li key={rule.id}>{rule.name}</li>)
+        : null}
+    </ol>
+  );
+};
 
 export default RulesSections;
