2847ffb6420fa43086a295149892901b1a79ec64
Guðjón Karl Arnarson <kromby@hress.org>
2024-05-31 21:38:17 +0000

    Sentry installation and config

--- src/index.js
+++ src/index.js
@@ -1,24 +1,43 @@
-import {StrictMode} from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-import config from 'react-global-configuration';
-import App from './pages/App';
-import dotenv from 'dotenv';
+import * as Sentry from "@sentry/react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import config from "react-global-configuration";
+import App from "./pages/App";
+import dotenv from "dotenv";
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/hress\.org/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 dotenv.config();
 
 function setConfig() {
-  config.set({
-    path: process.env.REACT_APP_LEGACY_API_PATH,
-    apiPath: process.env.REACT_APP_API_PATH ?? window.location.hostname, 
-    imagePath: process.env.REACT_APP_IMAGE_PATH,
-    code: process.env.REACT_APP_LEGACY_API_CODE,
-    omdb: process.env.REACT_APP_OMDB
-  }, {
-    freeze: false,
-    environment: 'prod'
-  });
+  config.set(
+    {
+      path: process.env.REACT_APP_LEGACY_API_PATH,
+      apiPath: process.env.REACT_APP_API_PATH ?? window.location.hostname,
+      imagePath: process.env.REACT_APP_IMAGE_PATH,
+      code: process.env.REACT_APP_LEGACY_API_CODE,
+      omdb: process.env.REACT_APP_OMDB,
+    },
+    {
+      freeze: false,
+      environment: "prod",
+    }
+  );
   config.set({});
 
   config.setEnvironment("prod");
--- src/pages/news/index.js
+++ src/pages/news/index.js
@@ -1,55 +1,87 @@
 import { useEffect, useState } from "react";
-import {isMobile} from 'react-device-detect';
+import { isMobile } from "react-device-detect";
 import axios from "axios";
-import config from 'react-global-configuration';
+import config from "react-global-configuration";
 import { Post } from "../../components";
 
 const News = () => {
-    const[news, setNews] = useState();    
+  const [news, setNews] = useState();
 
-    useEffect(() => {
-        const getNews = async () => {
-            const url = config.get("apiPath") + "/api/news";
-            try {
-                const response = await axios.get(url);
-                setNews(response.data);
-            }
-            catch(e) {
-                console.error(e);
-            }
-        }
+  useEffect(() => {
+    const getNews = async () => {
+      const url = config.get("apiPath") + "/api/news";
+      try {
+        const response = await axios.get(url);
+        setNews(response.data);
+      } catch (e) {
+        console.error(e);
+      }
+    };
 
-        document.title = "Forsíða | Hress.Org";
+    document.title = "Forsíða | Hress.Org";
 
-        if(!news) {
-            getNews();
-        }
-    }, [])
+    if (!news) {
+      getNews();
+    }
+  }, []);
 
-    return (
-        <div id="main">
-            {news ? news.map(singleNews => 
-                <Post key={singleNews.id}
-                    id={singleNews.id}
-                    href={"/news/" + singleNews.id}
-                    title={singleNews.name}
-                    date={singleNews.inserted}
-                    dateFormatted={singleNews.insertedString}
-                    author={singleNews.author}
-                    body={
-                        <p style={singleNews.image && singleNews.imageAlign !== 4 ? { "minHeight": singleNews.image.height-50 } : null}>
-                            {!isMobile && singleNews.imageAlign !== 4 ?
-                            <span className={singleNews.imageAlign === 1 ? "image left" : singleNews.imageAlign === 2 ? "image right" : null}>
-                                <img style={{"maxHeight": "500px"}} src={config.get("apiPath") + singleNews.image.href} alt={singleNews.name} />
-                            </span> : null }                            
-                            <span dangerouslySetInnerHTML={{ __html: singleNews.content }} /> {/* skipcq: JS-0440 */}
-                        </p>}
-                    image={isMobile || singleNews.imageAlign === 4 ? config.get('apiPath') + singleNews.image.href + "?width=1400" : null}
-                    actions = {<p />}
-                />
-            ) : null}
-        </div>
-    )
-}
+  return (
+    <div id="main">
+      <button onClick={() => methodDoesNotExist()}>Break the world</button>;
+      {news
+        ? news.map((singleNews) => (
+            <Post
+              key={singleNews.id}
+              id={singleNews.id}
+              href={"/news/" + singleNews.id}
+              title={singleNews.name}
+              date={singleNews.inserted}
+              dateFormatted={singleNews.insertedString}
+              author={singleNews.author}
+              body={
+                <p
+                  style={
+                    singleNews.image && singleNews.imageAlign !== 4
+                      ? { minHeight: singleNews.image.height - 50 }
+                      : null
+                  }
+                >
+                  {!isMobile && singleNews.imageAlign !== 4 ? (
+                    <span
+                      className={
+                        singleNews.imageAlign === 1
+                          ? "image left"
+                          : singleNews.imageAlign === 2
+                          ? "image right"
+                          : null
+                      }
+                    >
+                      <img
+                        style={{ maxHeight: "500px" }}
+                        src={config.get("apiPath") + singleNews.image.href}
+                        alt={singleNews.name}
+                      />
+                    </span>
+                  ) : null}
+                  <span
+                    dangerouslySetInnerHTML={{ __html: singleNews.content }}
+                  />{" "}
+                  {/* skipcq: JS-0440 */}
+                </p>
+              }
+              image={
+                isMobile || singleNews.imageAlign === 4
+                  ? config.get("apiPath") +
+                    singleNews.image.href +
+                    "?width=1400"
+                  : null
+              }
+              actions={<p />}
+            />
+          ))
+        : null}
+    </div>
+  );
+};
 
 export default News;
