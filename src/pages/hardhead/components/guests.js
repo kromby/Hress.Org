605fa6b63d6883591f56db36c0a06ee947568111
Guðjón Karl Arnarson <kromby@hress.org>
2024-06-22 14:51:08 +0000

    Add logging and setting better descriptions of stars per night

--- src/pages/hardhead/components/guests.js
+++ src/pages/hardhead/components/guests.js
@@ -1,44 +1,52 @@
-import { useEffect, useState } from 'react';
-import config from 'react-global-configuration';
-import UserImage from '../../../components/users/userimage';
+import { useEffect, useState } from "react";
+import config from "react-global-configuration";
+import UserImage from "../../../components/users/userimage";
 import axios from "axios";
 
 const Guests = ({ hardheadID }) => {
-    const [error, setError] = useState();
-    const [guests, setGuests] = useState();
+  const [error, setError] = useState();
+  const [guests, setGuests] = useState();
 
-    useEffect(() => {
-        const getGuests = () => {
-            const url = `${config.get('apiPath')}/api/hardhead/${hardheadID}/guests`;
+  useEffect(() => {
+    const getGuests = () => {
+      const url = `${config.get("apiPath")}/api/hardhead/${hardheadID}/guests`;
 
-            axios.get(url)
-                .then((response) => setGuests(response.data))
-                .catch((ex) => {
-                    if (ex.response.status !== 404) {
-                        setError(ex);
-                        console.log("[Guests] Error retrieving guests");
-                    }
-                })
-        }
+      axios
+        .get(url)
+        .then((response) => setGuests(response.data))
+        .catch((ex) => {
+          console.error("[Guests] Error: ", ex);
+          if (ex.response.status !== 404) {
+            setError(ex);
+            console.log("[Guests] Error retrieving guests");
+          }
+        });
+    };
 
-        if (!guests) {
-            getGuests();
-        }
-    }, [hardheadID])
+    if (!guests) {
+      getGuests();
+    }
+  }, [hardheadID]);
 
-    return (
-        <section>
-            <h3>Gestir</h3>
-            <div className="row gtr-uniform">
-                {guests ? guests.map(guest =>
-                    <div className="col-2 align-center" key={guest.id}>
-                        <UserImage id={guest.id} username={guest.username} profilePhoto={guest.profilePhoto?.href} />
-                    </div>
-                ) : null}
-                {error ? error : null}
-            </div>
-        </section>
-    )
-}
+  return (
+    <section>
+      <h3>Gestir</h3>
+      <div className="row gtr-uniform">
+        {guests
+          ? guests.map((guest) => (
+              <div className="col-2 align-center" key={guest.id}>
+                <UserImage
+                  id={guest.id}
+                  username={guest.username}
+                  profilePhoto={guest.profilePhoto?.href}
+                />
+              </div>
+            ))
+          : null}
+        {error ? error : null}
+      </div>
+    </section>
+  );
+};
 
 export default Guests;
