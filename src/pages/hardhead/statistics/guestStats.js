fbb5f14909ba7ff2a4a8259a26d8ed0c9952e2c3
Guðjón Karl Arnarson <kromby@hress.org>
2024-10-21 20:02:35 +0000

    Reload issue fixed

--- src/pages/hardhead/statistics/guestStats.js
+++ src/pages/hardhead/statistics/guestStats.js
@@ -1,108 +1,141 @@
-import { useState, useEffect } from 'react';
-import config from 'react-global-configuration';
-import axios from 'axios';
-import { Post } from '../../../components';
-import Author from '../../../components/author';
+import { useState, useEffect } from "react";
+import config from "react-global-configuration";
+import axios from "axios";
+import { Post } from "../../../components";
+import Author from "../../../components/author";
 
 const GuestStats = () => {
-    const [data, setData] = useState({ stats: null, isLoading: false, visible: false })
-    const [pageSize, setPageSize] = useState(10);
-    const [period, setPeriod] = useState("All");
-    const [reload, setReload] = useState(false);
+  const [data, setData] = useState({
+    stats: null,
+    isLoading: false,
+    visible: false,
+  });
+  const [pageSize, setPageSize] = useState(10);
+  const [period, setPeriod] = useState("All");
+  const [reload, setReload] = useState(false);
 
-    useEffect(() => {
-        const getStats = async () => {
-            try {
-                const url = config.get('apiPath') + '/api/hardhead/statistics/users?periodType=' + period + '&attendanceType=' + 52;
-                setData({ isLoading: true });
-                const response = await axios.get(url);
-                setData({ stats: response.data, isLoading: false, visible: true });
-            } catch (e) {
-                console.error(e);
-                setData({ isLoading: false, visible: false })
-            }
-        }
+  const url =
+    config.get("apiPath") +
+    "/api/hardhead/statistics/users?periodType=" +
+    period +
+    "&attendanceType=" +
+    52;
 
-        if (!data.stats || reload) {
-            getStats();
-            setReload(false);
-        }
-    }, [])
+  useEffect(() => {
+    const getStats = async () => {
+      try {
+        setData({ isLoading: true });
+        const response = await axios.get(url);
+        setData({ stats: response.data, isLoading: false, visible: true });
+      } catch (e) {
+        console.error(e);
+        setData({ isLoading: false, visible: false });
+      }
+    };
 
-    const handleSubmit = () => {
-        if (pageSize > data.stats.list.length) {
-            setPageSize(10);
-        } else {
-            setPageSize(pageSize + 10);
-        }
+    if (!data.stats || reload) {
+      getStats();
+      setReload(false);
+    }
+  }, [url]);
+
+  const handleSubmit = () => {
+    if (pageSize > data.stats.list.length) {
+      setPageSize(10);
+    } else {
+      setPageSize(pageSize + 10);
+    }
+  };
+
+  const getButtonText = () => {
+    if (
+      data.stats === undefined ||
+      data.stats === null ||
+      pageSize > data.stats.list.length
+    ) {
+      return 10;
     }
 
-    const getButtonText = () => {
-        if (data.stats === undefined || data.stats === null || pageSize > data.stats.list.length) {
-            return 10;
-        }
+    return pageSize + 10;
+  };
 
-        return pageSize + 10;
-    }
+  const handlePeriodChange = (event) => {
+    setPeriod(event.target.value);
+    setReload(true);
+  };
 
-    const handlePeriodChange = (event) => {
-        setPeriod(event.target.value);
-        setReload(true);
-    }
-
-    return (
-        <Post
-            title="Hver hefur mætt á flest harðhausakvöld"
-            description={
-                <select id="demo-category" name="demo-category" onChange={(ev) => handlePeriodChange(ev)}>
-                    <option value="All">frá upphafi</option>
-                    <option value="Last10">síðustu 10 ár</option>
-                    <option value="Last5">síðustu 5 ár</option>
-                    <option value="Last2">síðustu 2 ár</option>
-                    <option value="ThisYear">þetta ár</option>
-                </select>
-            }
-            date={data.visible ? data.stats.dateFrom : null}
-            dateFormatted={data.visible ? data.stats.dateFromString : null}
-            showFooter={false}
-            body={data.visible ?
-                <div className="table-wrapper">
-                    <table>
-                        <thead>
-                            <tr>
-                                <td width="100px">Nr.</td>
-                                <td width="250px">Harðhaus</td>
-                                <td>Fjöldi</td>
-                                <td>Fyrst mætt</td>
-                                <td>Síðast mætt</td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {data.stats.list.slice(0, pageSize).map((stat, i) =>
-                                <tr key={stat.user.id}>
-                                    <td>{i + 1}</td>
-                                    <td>
-                                        {typeof stat.user.profilePhoto !== 'undefined' && stat.user.profilePhoto !== null ?
-                                            <Author ID={stat.user.id} Username={stat.user.username} UserPath="/hardhead/users/" ProfilePhoto={stat.user.profilePhoto.href} /> :
-                                            <Author ID={stat.user.id} Username={stat.user.username} UserPath="/hardhead/users/" />
-                                        }
-                                    </td>
-                                    <td>{stat.attendedCount}</td>
-                                    <td>{stat.firstAttendedString}</td>
-                                    <td>{stat.lastAttendedString}</td>
-                                </tr>
-                            )}
-                        </tbody>
-                    </table>
-                </div>
-                : null}
-            actions={
-                <div>
-                    <button className="button large" onClick={handleSubmit}>Sjá {getButtonText()} efstu</button>
-                </div>
-            }
-        />
-    )
-}
+  return (
+    <Post
+      title="Hver hefur mætt á flest harðhausakvöld"
+      description={
+        <select
+          id="demo-category"
+          name="demo-category"
+          onChange={(ev) => handlePeriodChange(ev)}
+        >
+          <option value="All">frá upphafi</option>
+          <option value="Last10">síðustu 10 ár</option>
+          <option value="Last5">síðustu 5 ár</option>
+          <option value="Last2">síðustu 2 ár</option>
+          <option value="ThisYear">þetta ár</option>
+        </select>
+      }
+      date={data.visible ? data.stats.dateFrom : null}
+      dateFormatted={data.visible ? data.stats.dateFromString : null}
+      showFooter={false}
+      body={
+        data.visible ? (
+          <div className="table-wrapper">
+            <table>
+              <thead>
+                <tr>
+                  <td width="100px">Nr.</td>
+                  <td width="250px">Harðhaus</td>
+                  <td>Fjöldi</td>
+                  <td>Fyrst mætt</td>
+                  <td>Síðast mætt</td>
+                </tr>
+              </thead>
+              <tbody>
+                {data.stats.list.slice(0, pageSize).map((stat, i) => (
+                  <tr key={stat.user.id}>
+                    <td>{i + 1}</td>
+                    <td>
+                      {typeof stat.user.profilePhoto !== "undefined" &&
+                      stat.user.profilePhoto !== null ? (
+                        <Author
+                          ID={stat.user.id}
+                          Username={stat.user.username}
+                          UserPath="/hardhead/users/"
+                          ProfilePhoto={stat.user.profilePhoto.href}
+                        />
+                      ) : (
+                        <Author
+                          ID={stat.user.id}
+                          Username={stat.user.username}
+                          UserPath="/hardhead/users/"
+                        />
+                      )}
+                    </td>
+                    <td>{stat.attendedCount}</td>
+                    <td>{stat.firstAttendedString}</td>
+                    <td>{stat.lastAttendedString}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        ) : null
+      }
+      actions={
+        <div>
+          <button className="button large" onClick={handleSubmit}>
+            Sjá {getButtonText()} efstu
+          </button>
+        </div>
+      }
+    />
+  );
+};
 
 export default GuestStats;
