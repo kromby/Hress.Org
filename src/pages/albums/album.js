43cb1e04f048a5837f56d65c504558be5f84eeb8
Guðjón Karl Arnarson <kromby@hress.org>
2024-11-10 19:36:15 +0000

    Link in album to add images

--- src/pages/albums/album.js
+++ src/pages/albums/album.js
@@ -7,54 +7,57 @@ import { useAuth } from "../../context/auth";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 
 const Album = () => {
-    const { authTokens } = useAuth();
-    const location = useLocation();
-    const navigate = useNavigate();
-    const params = useParams();
-    const [album, setAlbum] = useState();
+  const { authTokens } = useAuth();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const params = useParams();
+  const [album, setAlbum] = useState();
 
-    useEffect(() => {
-        if (authTokens === undefined) {
-            navigate("/login", { state: { from: location.pathname } });
-            return;
-        }
+  useEffect(() => {
+    if (authTokens === undefined) {
+      navigate("/login", { state: { from: location.pathname } });
+      return;
+    }
 
-        const getAlbum = async () => {
-            const url = config.get("apiPath") + "/api/albums/" + params.id;
-            try {
-                const response = await axios.get(url, {
-                    headers: { 'X-Custom-Authorization': 'token ' + authTokens.token },
-                });
-                setAlbum(response.data);
-                document.title = "Myndir - " + response.data.name + " | Hress.Org";
-            } catch (e) {
-                console.error(e);
-            }
-        }
+    const getAlbum = async () => {
+      const url = config.get("apiPath") + "/api/albums/" + params.id;
+      try {
+        const response = await axios.get(url, {
+          headers: { "X-Custom-Authorization": "token " + authTokens.token },
+        });
+        setAlbum(response.data);
+        document.title = "Myndir - " + response.data.name + " | Hress.Org";
+      } catch (e) {
+        console.error(e);
+      }
+    };
 
-        if (!album) {
-            getAlbum();
-        }
-    }, []);
+    if (!album) {
+      getAlbum();
+    }
+  }, []);
 
-
-    return (
-        <div id="main">
-            {album ?
-                <Post key={album.id}
-                    id={album.id}
-                    title={album.name}
-                    description={album.description}
-                    dateFormatted={album.insertedString}
-                    body={
-                        <div>
-                            <AlbumImages url={album.images.href} />
-                        </div>
-                    }
-                /> : null
-            }
-        </div>
-    )
-}
+  return (
+    <div id="main">
+      {album ? (
+        <Post
+          key={album.id}
+          id={album.id}
+          title={album.name}
+          description={album.description}
+          dateFormatted={album.insertedString}
+          body={
+            <div>
+              <AlbumImages url={album.images.href} />
+              <a href={`/album/${album.id}/upload`}>
+                Bæta við myndum í þetta albúm
+              </a>
+            </div>
+          }
+        />
+      ) : null}
+    </div>
+  );
+};
 
 export default Album;
