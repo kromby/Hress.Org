34a96f393be93f03418e3890f73057345e9867cd
Guðjón Karl Arnarson <kromby@hress.org>
2024-11-29 17:44:45 +0000

    Changing to this year in nomination page... must remember to properly fix this

--- src/pages/hardhead/awards/nominations/index.tsx
+++ src/pages/hardhead/awards/nominations/index.tsx
@@ -1,48 +1,52 @@
-import { useEffect, useState } from 'react';
-import config from 'react-global-configuration';
+import { useEffect, useState } from "react";
+import config from "react-global-configuration";
 import axios from "axios";
-import { useAuth } from '../../../../context/auth';
-import StalloneNomination from './stalloneNomination';
-import DisappointmentNomination from './disappointmentNomination';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useAuth } from "../../../../context/auth";
+import StalloneNomination from "./stalloneNomination";
+import DisappointmentNomination from "./disappointmentNomination";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Nominations = () => {
-    const { authTokens } = useAuth();
-    const location = useLocation();
-    const [users, setUsers] = useState<any>();
-    const navigate = useNavigate();
+  const { authTokens } = useAuth();
+  const location = useLocation();
+  const [users, setUsers] = useState<any>();
+  const navigate = useNavigate();
 
-    const url = `${config.get('path')}/api/hardhead/5384/users?code=${config.get('code')}`;
+  const url = `${config.get("path")}/api/hardhead/5418/users?code=${config.get(
+    "code"
+  )}`;
 
-    useEffect(() => {
-        if (authTokens === undefined) {            
-            navigate("/login", { state: { from: location.pathname } });
-            return;
-        }
+  useEffect(() => {
+    if (authTokens === undefined) {
+      navigate("/login", { state: { from: location.pathname } });
+      return;
+    }
 
-        const getUsers = async () => {
-            try {
-                const userID = localStorage.getItem("userID");
-                const response = await axios.get(url);
-                setUsers(response.data.filter((user: { ID: string }) => user.ID !== userID));
-            } catch (e) {
-                console.error(e);
-            }
-        }
+    const getUsers = async () => {
+      try {
+        const userID = localStorage.getItem("userID");
+        const response = await axios.get(url);
+        setUsers(
+          response.data.filter((user: { ID: string }) => user.ID !== userID)
+        );
+      } catch (e) {
+        console.error(e);
+      }
+    };
 
-        document.title = "Tilnefningar | Hress.Org";
+    document.title = "Tilnefningar | Hress.Org";
 
-        if (!users) {
-            getUsers();
-        }
-    }, [url])
+    if (!users) {
+      getUsers();
+    }
+  }, [url]);
 
-    return (
-        <div id="main">
-            <StalloneNomination Users={users} Type="5284" />
-            <DisappointmentNomination Users={users} Type="360" />
-        </div>
-    )
-}
+  return (
+    <div id="main">
+      <StalloneNomination Users={users} Type="5284" />
+      <DisappointmentNomination Users={users} Type="360" />
+    </div>
+  );
+};
 
 export default Nominations;
