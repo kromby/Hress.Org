3a855e1a4243724b1c0c3b0e7c0528ce3c09ec84
Guðjón Karl <kromby@hress.org>
2024-11-30 14:29:35 +0000

    Packages upgraded, sentry, types and typescript (#413)

---
+++ src/pages/hardhead/awards/election/votenow.tsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import { Post } from "../../../../components";
+import { useAuth } from "../../../../context/auth";
+
+const VoteNow = () => {
+  const { authTokens } = useAuth();
+
+  return (
+    <div>
+      {authTokens ? (
+        <Post
+          id="0"
+          title="Harðhausakosningin"
+          dateFormatted="2024"
+          body={
+            <section>
+              <p>
+                <Link to="/hardhead/awards/election" className="button large">
+                  Kjósa núna!
+                </Link>
+              </p>
+            </section>
+          }
+        />
+      ) : null}
+    </div>
+  );
+};
+
+export default VoteNow;
