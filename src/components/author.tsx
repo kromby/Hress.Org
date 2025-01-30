bd9386424f06e8f5566e70b0dc5f722e2e617637
Guðjón Karl Arnarson <kromby@hress.org>
2025-01-30 21:21:17 +0000

    Using new useSingleUser hook

--- src/components/author.tsx
+++ src/components/author.tsx
@@ -1,64 +1,56 @@
-import axios from "axios";
 import config from "react-global-configuration";
-import { useEffect, useState } from "react";
-import { TransactionUser } from "../types/balanceSheet";
+import { useEffect, useState, useMemo } from "react";
+import { useSingleUser } from "../hooks/useSingleUser";
+import { UserBasicEntity } from "../types/userBasicEntity";
 
 interface AuthorProps {
   id: number;
   username: string;
-  href?: string | null;
-  profilePhoto?: string | null;
-  userPath?: string | null;
+  href?: string | undefined;
+  profilePhoto?: string | undefined;
+  userPath?: string | undefined;
 }
 
 const Author: React.FC<AuthorProps> = ({
   id,
   username,
-  href = null,
-  profilePhoto = null,
-  userPath = null,
+  href = undefined,
+  profilePhoto = undefined,
+  userPath = undefined,
 }) => {
-  const [user, setUser] = useState<TransactionUser>();
+  const { user: fetchedUser, loading, error } = useSingleUser(href);
   const [userURL, setUserURL] = useState(
     "http://www.hress.org/Gang/Single.aspx?Id="
   );
 
-  useEffect(() => {
-    let isMounted = true;
-    const getUser = async () => {
-      if (href) {
-        const url = config.get("apiPath") + href;
-        try {
-          const response = await axios.get(url);
-          if(isMounted) {
-            setUser(response.data);
-          }
-        } catch (error) {
-          console.error("Error fetching user:", error);
-        }
-      } else {
-        if(isMounted) {
-        setUser({
-          id,
-          username,
-          href: "",
-          name: username,
-          profilePhoto: {
-            id: 0,
-            href: profilePhoto,
-          },
-        });
-        }
-      }
-    };
-    getUser();
-
-    if (userPath) setUserURL(userPath);
-
-    return () => {
-      isMounted = false;
+  const user = useMemo<UserBasicEntity | undefined>(() => {
+    if (href) {
+      return fetchedUser;
     }
-  }, [href, id, username, profilePhoto, userPath]);
+    
+    return {
+      id,
+      username,
+      href: "",
+      name: username,
+      profilePhoto: profilePhoto ? {
+        id: 0,
+        href: profilePhoto,
+      } : undefined,
+      inserted: new Date().toString(),
+      insertedString: "",
+      insertedBy: 0,
+      deleted: "",
+    };
+  }, [fetchedUser, href, id, username, profilePhoto]);
+
+  useEffect(() => {
+    if (userPath) setUserURL(userPath);
+  }, [userPath]);
+
+  if (href && loading) return <div>Loading...</div>;
+  if (href && error) return <div>Error loading user</div>;
+  if (!user) return null;
 
   const getImageSrc = (profilePhoto: string, mode: string) => {
     const size = mode === "Expanded" ? 50 : mode === "Compact" ? 25 : 40;
@@ -70,18 +62,15 @@ const Author: React.FC<AuthorProps> = ({
 
   return (
     <div>
-      {user ? (
-        <a href={userURL + user.id} className="author">
-          <span className="name">{user.username}</span>
+      <a href={userURL + user.id} className="author">
+        <span className="name">{user.username}</span>
 
         <img
           src={getImageSrc(user.profilePhoto?.href ?? '', "Expanded")}
           alt={username}
           className="profile-photo"
         />
-        </a>
-
-        ) : null}
+      </a>
     </div>
   );
 };
