339f3a353235f8b3d148d12f30a74a95145ed485
Guðjón Karl Arnarson <kromby@hress.org>
2025-02-02 14:45:44 +0000

    API migrated and called through hook

---
+++ src/hooks/useAward.ts
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+import axios, { AxiosError } from 'axios';
+import config from "react-global-configuration";
+import { Award } from '../types/award';
+
+export const useAward = (awardId: number) => {
+    const [award, setAward] = useState<Award | null>(null);
+    const [error, setError] = useState<AxiosError | null>(null);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        const fetchAward = async () => {
+            if (!awardId) return;
+            
+            setIsLoading(true);
+            try {
+                const url = `${config.get('apiPath')}/api/hardhead/awards/${awardId}`;
+                const response = await axios.get(url);
+                setAward(response.data);
+            } catch (e) {
+                if (axios.isAxiosError(e)) {
+                    console.error("Failed to fetch user:", e.message);
+                    setError(e);
+                  } else {
+                    console.error("An unexpected error occurred:", e);
+                    setError(new AxiosError("An unexpected error occurred"));
+                  }
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchAward();
+    }, [awardId]);
+
+    return { award, error, isLoading };
+};
--- src/types/award.ts
+++ src/types/award.ts
@@ -1,7 +1,17 @@
 import { EntityBase } from "./entityBase";
 import { HrefEntity } from "./hrefEntity";
+import { UserBasicEntity } from "./userBasicEntity";
+
+export interface YearEntity extends EntityBase<number> {
+    guestCount: number;
+    description: string;
+    photoId?: number;
+    photo?: HrefEntity;
+    hardhead: UserBasicEntity;
+}
 
 export interface Award extends EntityBase<number> {
-    winners: HrefEntity
-    href?: string
-} 
+    winners: HrefEntity;
+    href?: string;
+    years: YearEntity[];
+}
