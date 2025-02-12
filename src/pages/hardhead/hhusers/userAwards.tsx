d9949b6433fcbf1496c43acc5f3ec23dece0123e
Guðjón Karl Arnarson <kromby@hress.org>
2025-02-12 21:57:05 +0000

    Correct typing

--- src/pages/hardhead/hhusers/userAwards.tsx
+++ src/pages/hardhead/hhusers/userAwards.tsx
@@ -3,38 +3,38 @@ import UserAwardDetail from "./userAwardDetail";
 import { useAwards } from "../../../hooks/hardhead/useAwards";
 
 interface UserAwardsProps {
-    id: number;
+  id: number;
 }
 
 const UserAwards = ({ id }: UserAwardsProps) => {
-    const { awards, error, isLoading } = useAwards();
+  const { awards, error, isLoading } = useAwards();
 
-    return (
-        <Post 
-            title="Verðlaunin"
-            description="Efstu þrjú sætin"
-            body={
-                <section>
-                    {isLoading ? (
-                        <div className="loading">Sæki verðlaun...</div>
-                    ) : error ? (
-                        <div className="error">Villa kom upp við að sækja verðlaun</div>
-                    ) : awards?.length ? (
-                        awards.map(award => 
-                            <UserAwardDetail 
-                                key={award.id} 
-                                awardID={award.id} 
-                                name={award.name} 
-                                userID={id} 
-                            />
-                        )
-                    ) : (
-                        <div>Engin verðlaun fundust</div>
-                    )}
-                </section>
-            }
-        />
-    );
-}
+  return (
+    <Post
+      title="Verðlaunin"
+      description="Efstu þrjú sætin"
+      body={
+        <section>
+          {isLoading ? (
+            <div className="loading">Sæki verðlaun...</div>
+          ) : error ? (
+            <div className="error">Villa kom upp við að sækja verðlaun</div>
+          ) : awards?.length ? (
+            awards.map((award) => (
+              <UserAwardDetail
+                key={award.id}
+                awardID={award.id}
+                name={award.name || ""}
+                userID={id}
+              />
+            ))
+          ) : (
+            <div>Engin verðlaun fundust</div>
+          )}
+        </section>
+      }
+    />
+  );
+};
 
 export default UserAwards;
