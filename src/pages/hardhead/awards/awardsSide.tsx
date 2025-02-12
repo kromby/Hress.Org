75b54af54b73737abdc1bcd7ccab2ce510962bc2
Guðjón Karl Arnarson <kromby@hress.org>
2025-02-12 19:54:42 +0000

    Require template literals instead of string concatenation JS-0246

--- src/pages/hardhead/awards/awardsSide.tsx
+++ src/pages/hardhead/awards/awardsSide.tsx
@@ -1,45 +1,58 @@
-import { useState, useEffect } from 'react';
-import config from 'react-global-configuration';
-import { MiniPost } from '../../../components';
-import axios from 'axios';
-import { WinnerEntity } from '../../../types/winnerEntity';
+import { useState, useEffect } from "react";
+import config from "react-global-configuration";
+import { MiniPost } from "../../../components";
+import axios from "axios";
+import { WinnerEntity } from "../../../types/winnerEntity";
 
 const AwardsSide = () => {
-    const [data, setData] = useState({ isLoading: false, visible: false })  
-    const [awards, setAwards] = useState<WinnerEntity>()
+  const [data, setData] = useState({ isLoading: false, visible: false });
+  const [awards, setAwards] = useState<WinnerEntity>();
 
-    useEffect(() => {
-        const getAwards = async () => {
-            try {
-                const url = `${config.get('apiPath')}/api/hardhead/awards/364/winners?position=1`;
-                setData({ isLoading: true, visible: false });
-                const response = await axios.get(url);
-                setData({ isLoading: false, visible: true });
-                setAwards(response.data[0]);
-            } catch (e) {
-                console.error(e);
-                setData({ isLoading: false, visible: false });
-            }
-        }
+  useEffect(() => {
+    const getAwards = async () => {
+      try {
+        const url = `${config.get(
+          "apiPath"
+        )}/api/hardhead/awards/364/winners?position=1`;
+        setData({ isLoading: true, visible: false });
+        const response = await axios.get(url);
+        setData({ isLoading: false, visible: true });
+        setAwards(response.data[0]);
+      } catch (e) {
+        console.error(e);
+        setData({ isLoading: false, visible: false });
+      }
+    };
 
-        if (!awards) {
-            getAwards();
-        }
-    }, [])
+    if (!awards) {
+      getAwards();
+    }
+  }, []);
 
-    return (
-        <div>
-            {data.visible && awards ?
-                <MiniPost title="Harðhausa verðlaunin" href="/hardhead/awards"
-                    description={<span>Harðhaus ársins<br />{awards.winner.username} með {awards.value} atkvæði</span>}
-                    date={"1.1." + awards.year}
-                    dateString={awards.year.toString()}
-                    userHref={"/hardhead/users/" + awards.winner.id}
-                    userPhoto={config.get('path') + awards.winner.profilePhoto?.href + "?code=" + config.get('code')}
-                    userText={awards.winner.username} /> :
-                null}
-        </div>
-    )
-}
+  return (
+    <div>
+      {data.visible && awards ? (
+        <MiniPost
+          title="Harðhausa verðlaunin"
+          href="/hardhead/awards"
+          description={
+            <span>
+              Harðhaus ársins
+              <br />
+              {awards.winner.username} með {awards.value} atkvæði
+            </span>
+          }
+          date={`1.1.${awards.year}`}
+          dateString={awards.year.toString()}
+          userHref={`/hardhead/users/${awards.winner.id}`}
+          userPhoto={`${config.get("path")}${
+            awards.winner.profilePhoto?.href
+          }?code=${config.get("code")}`}
+          userText={awards.winner.username}
+        />
+      ) : null}
+    </div>
+  );
+};
 
 export default AwardsSide;
