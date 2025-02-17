33d06e964b501fc78defcfe46cdd200ed9df6ae0
Guðjón Karl Arnarson <kromby@hress.org>
2025-02-17 20:57:36 +0000

    Split into two hooks... byId and byHref

--- src/hooks/hardhead/useAwardWinners.ts
+++ src/hooks/hardhead/useAwardWinners.ts
@@ -3,10 +3,11 @@ import config from "react-global-configuration";
 import axios, { AxiosError } from "axios";
 import { WinnerEntity } from "../../types/winnerEntity";
 
-export const useAwardWinners = (
+export const useAwardWinnersById = (
   awardId: number,
   userId?: number,
-  position?: number
+  position?: number,
+  year?: number
 ) => {
   const [winners, setWinners] = useState<WinnerEntity[]>();
   const [error, setError] = useState<AxiosError | null>(null);
@@ -22,6 +23,7 @@ export const useAwardWinners = (
         const params = new URLSearchParams();
         if (userId) params.append("user", userId.toString());
         if (position) params.append("position", position.toString());
+        if (year) params.append("year", year.toString());
         const url = `${baseUrl}${
           params.toString() ? `?${params.toString()}` : ""
         }`;
@@ -42,7 +44,51 @@ export const useAwardWinners = (
     };
 
     fetchWinners();
-  }, [awardId, userId, position]);
+  }, [awardId, userId, position, year]);
+
+  return { winners, error, isLoading };
+};
+
+export const useAwardWinnersByHref = (
+  href: string,
+  userId?: number,
+  position?: number,
+  year?: number
+) => {
+  const [winners, setWinners] = useState<WinnerEntity[]>();
+  const [error, setError] = useState<AxiosError | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchWinners = async () => {
+      setIsLoading(true);
+      try {
+        const baseUrl = `${config.get("apiPath")}${href}`;
+        const params = new URLSearchParams();
+        if (userId) params.append("user", userId.toString());
+        if (position) params.append("position", position.toString());
+        if (year) params.append("year", year.toString());
+        const url = `${baseUrl}${
+          params.toString() ? `?${params.toString()}` : ""
+        }`;
+
+        const response = await axios.get(url);
+        setWinners(response.data);
+      } catch (e) {
+        if (axios.isAxiosError(e)) {
+          console.error("Failed to fetch winners:", e.message);
+          setError(e);
+        } else {
+          console.error("An unexpected error occurred:", e);
+          setError(new AxiosError("An unexpected error occurred"));
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchWinners();
+  }, [href, userId, position, year]);
 
   return { winners, error, isLoading };
 };
