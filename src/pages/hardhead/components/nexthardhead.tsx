2872131480d7e7aad1d4de47972c14c222d2e125
Guðjón Karl Arnarson <kromby@hress.org>
2025-02-22 08:44:23 +0000

    Typescript

--- src/pages/hardhead/components/nexthardhead.tsx
+++ src/pages/hardhead/components/nexthardhead.tsx
@@ -3,11 +3,12 @@ import config from "react-global-configuration";
 import MiniPost from "../../../components/minipost";
 import HardheadActions from "./actions";
 import { useHardhead } from "../../../hooks/hardhead/useHardhead";
+import { HardheadNight } from "../../../types/hardheadNight";
 
-const NextHardhead = ({ allowEdit }) => {
+const NextHardhead = ({ allowEdit }: { allowEdit: boolean }) => {
   const { fetchNextHardhead } = useHardhead();
   const [editEnabled, setEditEnabled] = useState(false);
-  const [hardhead, setHardhead] = useState(null);
+  const [hardhead, setHardhead] = useState<HardheadNight | null>(null);
 
   useEffect(() => {
     const loadNextHardhead = async () => {
@@ -38,8 +39,10 @@ const NextHardhead = ({ allowEdit }) => {
           }
           dateString={hardhead.dateString}
           date={hardhead.date}
-          userHref={"/hardhead/users/" + hardhead.host.id}
-          userPhoto={config.get("apiPath") + hardhead.host.profilePhoto.href}
+          userHref={`/hardhead/users/${hardhead.host.id}`}
+          userPhoto={`${config.get("apiPath")}${
+            hardhead.host.profilePhoto?.href
+          }`}
           userText={hardhead.host.username}
         />
       ) : null}
