90f397ca22cd9d80450b8a76146894dcd27256c0
Guðjón Karl Arnarson <kromby@hress.org>
2025-02-22 08:37:59 +0000

    fetchNextHardhead hook and using it

--- src/pages/hardhead/components/nexthardhead.js
+++ src/pages/hardhead/components/nexthardhead.js
@@ -1,47 +1,50 @@
-import {useState, useEffect} from 'react';
-import config from 'react-global-configuration';
-import axios from "axios";
-import MiniPost from '../../../components/minipost';
-import HardheadActions from './actions';
+import { useState, useEffect } from "react";
+import config from "react-global-configuration";
+import MiniPost from "../../../components/minipost";
+import HardheadActions from "./actions";
+import { useHardhead } from "../../../hooks/hardhead/useHardhead";
 
-const NextHardhead = ({allowEdit}) => {
-    const[hardhead, setHardhead] = useState();
-    const[editEnabled, setEditEnabled] = useState(false);    
+const NextHardhead = ({ allowEdit }) => {
+  const { fetchNextHardhead } = useHardhead();
+  const [editEnabled, setEditEnabled] = useState(false);
+  const [hardhead, setHardhead] = useState(null);
 
-    useEffect(() => {
-        const getNextHardhead = async () => {
-            try {
-                const url = `${config.get('path')}/api/hardhead?code=${config.get('code')}`;		
-                const response = await axios.get(url);
+  useEffect(() => {
+    const loadNextHardhead = async () => {
+      try {
+        const data = await fetchNextHardhead();
+        setHardhead(data[0]);
+      } catch (error) {
+        console.error("Failed to fetch next hardhead:", error);
+      }
+    };
 
-                if(response.data.length > 0) {
-                    setHardhead(response.data[0]);
-                }
-            }
-            catch(e) {
-                console.error(e);
-            }
-        };
+    loadNextHardhead();
+    setEditEnabled(allowEdit);
+  }, [allowEdit]);
 
-        setEditEnabled(allowEdit);
-
-        if(!hardhead) {
-            getNextHardhead();
-        }
-    }, [allowEdit])
-
-    return (
-        <div>
-            {hardhead ? 
-                <MiniPost title="Næsta harðhausakvöld" 
-                    description={<span>{hardhead.Host.Username}<br/><br/>{editEnabled ? <HardheadActions id={hardhead.ID}/>:null}</span>}
-                    dateString={hardhead.DateString} 
-                    date={hardhead.Date}
-                    userHref={"/hardhead/users/" + hardhead.Host.ID}
-                    userPhoto={config.get('apiPath') + hardhead.Host.ProfilePhoto.Href}
-                    userText={hardhead.Host.Username} /> : null}        
-        </div>
-    )
-}
+  return (
+    <div>
+      {hardhead ? (
+        <MiniPost
+          title="Næsta harðhausakvöld"
+          description={
+            <span>
+              {hardhead.host.username}
+              <br />
+              <br />
+              {editEnabled ? <HardheadActions id={hardhead.id} /> : null}
+            </span>
+          }
+          dateString={hardhead.dateString}
+          date={hardhead.date}
+          userHref={"/hardhead/users/" + hardhead.host.id}
+          userPhoto={config.get("apiPath") + hardhead.host.profilePhoto.href}
+          userText={hardhead.host.username}
+        />
+      ) : null}
+    </div>
+  );
+};
 
 export default NextHardhead;
