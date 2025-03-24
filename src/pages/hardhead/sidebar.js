6a3fcc21d670fdd0e8bcb1c7e6325a48e57e6741
Guðjón Karl <kromby@hress.org>
2025-03-24 21:14:56 +0000

    Bug/524/fetch years (#525)
    * Icelandic and not displaying 404 axios error
    
    * Typescript convertion
    
    * Testing API
    
    * Splitting into two files
    
    * Creating /hardhead/years endpoint
    
    * API call moved to hook
    
    * Typescript, using hook, etc
    
    * Fixing display of ul tag
    
    * C:/Program Files/Git/hardhead/{yearId}/users endpoint added
    
    * API
    
    * Author replaces AuthorOld and Users type added
    
    * Data retrieval moved to hook
    
    * Package upgrades
    
    * SQL fixed
    
    * Linking to new movie overview page

--- src/pages/hardhead/sidebar.js
+++ src/pages/hardhead/sidebar.js
@@ -1,80 +1,96 @@
-import { Intro } from '../../components';
-import './sidebar.css';
-import NextHardhead from './components/nexthardhead';
-import AwardsSide from './awards/awardsSide';
-import StatisticSide from './statistics/statisticsSide';
-import RulesSide from './rules/rulesSide';
-import YearsSide from './components/yearsSide';
-import { Link } from 'react-router-dom';
+import { Intro } from "../../components";
+import "./sidebar.css";
+import NextHardhead from "./components/nexthardhead";
+import AwardsSide from "./awards/awardsSide";
+import StatisticSide from "./statistics/statisticsSide";
+import RulesSide from "./rules/rulesSide";
+import YearsSide from "./components/yearsSide";
+import { Link } from "react-router-dom";
 
 const HardheadSidebar = () => {
-	return (
-		<section id="sidebar">
-			<Intro logo="/logo.png" title="Harðhaus" description="" />
+  return (
+    <section id="sidebar">
+      <Intro logo="/logo.png" title="Harðhaus" description="" />
 
-			{/* <!-- Mini Posts --> */}
-			<section>
-				<div className="mini-posts">
-					{/* <!-- Mini Post --> */}
-					<NextHardhead allowEdit="true" />
-					{/* <MiniPost 
+      {/* <!-- Mini Posts --> */}
+      <section>
+        <div className="mini-posts">
+          {/* <!-- Mini Post --> */}
+          <NextHardhead allowEdit="true" />
+          {/* <MiniPost 
 						title="Harðhausabústaður"
 						dateString="19. janúar"						
 					/> */}
-					<AwardsSide />
-					<StatisticSide />
-					<RulesSide />
-				</div>
-			</section>
-			{/* <!-- Posts List --> */}
-			<section>
-				<ul className="posts">
-					<YearsSide />
-				</ul>
-			</section>
+          <AwardsSide />
+          <StatisticSide />
+          <RulesSide />
+        </div>
+      </section>
+      {/* <!-- Posts List --> */}
+      <section>
+        <ul className="posts">
+          <YearsSide />
+        </ul>
+      </section>
 
-			{/* <!-- About --> */}
-			<section className="blurb">
-				<h2>Um Harðhaus</h2>
-				<p>Harðhausakvöld eru haldin einu sinni í mánuði. Á kvöldinu er horft á harðhausamynd.</p>
-			</section>
+      {/* <!-- About --> */}
+      <section className="blurb">
+        <h2>Um Harðhaus</h2>
+        <p>
+          Harðhausakvöld eru haldin einu sinni í mánuði. Á kvöldinu er horft á
+          harðhausamynd.
+        </p>
+      </section>
 
-			<section>
-				<ul className="posts">
-					<li>
-						<article>
-							<header>
-								<Link to={"/Hardhead/films.aspx"} target="_parent">
-									<h3>Myndirnar</h3>
-								</Link>
-							</header>
-						</article>
-					</li>
-					<li>
-						<article>
-							<header>
-								<Link to={"/Hardhead/defaultold.aspx"} target="_parent">
-									<h3>Gamla harðhausasíðan</h3>
-								</Link>
-							</header>
-						</article>
-					</li>
-				</ul>
-			</section>
+      <section>
+        <ul className="posts">
+          <li>
+            <article>
+              <header>
+                <Link to={"/Hardhead/list"} target="_parent">
+                  <h3>Myndirnar</h3>
+                </Link>
+              </header>
+            </article>
+          </li>
+          <li>
+            <article>
+              <header>
+                <Link to={"/Hardhead/defaultold.aspx"} target="_parent">
+                  <h3>Gamla harðhausasíðan</h3>
+                </Link>
+              </header>
+            </article>
+          </li>
+        </ul>
+      </section>
 
-			{/* <!-- Footer --> */}
-			<section id="footer">
-				<ul className="icons">
-					{/* <li><a href="#" className="icon brands fa-twitter"><span className="label">Twitter</span></a></li> */}
-					<li><a href="https://www.facebook.com/groups/988193164623754/" className="icon brands fa-facebook-f" target="_blank" rel="noopener noreferrer"><span className="label">Facebook</span></a></li>
-					{/* <li><a href="#" className="icon brands fa-instagram"><span className="label">Instagram</span></a></li>
+      {/* <!-- Footer --> */}
+      <section id="footer">
+        <ul className="icons">
+          {/* <li><a href="#" className="icon brands fa-twitter"><span className="label">Twitter</span></a></li> */}
+          <li>
+            <a
+              href="https://www.facebook.com/groups/988193164623754/"
+              className="icon brands fa-facebook-f"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span className="label">Facebook</span>
+            </a>
+          </li>
+          {/* <li><a href="#" className="icon brands fa-instagram"><span className="label">Instagram</span></a></li>
 					<li><a href="#" className="icon solid fa-rss"><span className="label">RSS</span></a></li>
 					<li><a href="#" className="icon solid fa-envelope"><span className="label">Email</span></a></li> */}
-				</ul>
-				<p className="copyright">&copy; Hress.Org - Guðjón Karl Arnarson.<br />Design: <a href="http://html5up.net">HTML5 UP</a>.</p>
-			</section>
-		</section>
-	);
-}
+        </ul>
+        <p className="copyright">
+          &copy; Hress.Org - Guðjón Karl Arnarson.
+          <br />
+          Design: <a href="http://html5up.net">HTML5 UP</a>.
+        </p>
+      </section>
+    </section>
+  );
+};
 
 export default HardheadSidebar;
