25bbebef62810dc18d01c26597500d73aae55491
Guðjón Karl <kromby@hress.org>
2026-01-09 22:20:25 +0000

    Unused import removed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import * as Sentry from "@sentry/react";
-import React, { StrictMode } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import "./index.css";
